66f2bef4830726d5166ccce8bdedcc29c16f2a95
imudasirali <mudasir.ali@axelerant.com>
2025-02-07 16:41:24 +0530

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,23 +7,32 @@ const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
             <p>My cool posts will go in here</p>
-            <ul>
-                {data.allFile.nodes.map((node) => (
-                    <li key={node.name}>{node.name}</li>
-                ))}
-            </ul>
+            {
+                data.allMdx.nodes.map((node) => (
+                <article key={node.id}>
+                    <h2>{node.frontmatter.title}</h2>
+                    <p>Posted: {node.frontmatter.date}</p>
+                    <p>{node.excerpt}</p>
+                </article>
+                ))
+            }
         </Layout>
     )
 }
 
 export const data = graphql`
-query {
-  allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
-    nodes {
-      name
+  query {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
+      }
     }
   }
-}
 `
 
 export const Head = () => <Seo title="My Blog Posts" />
