0d407b9e8587d1ee4f27d39f73b7ff757844fecb
imudasirali <mudasir.ali@axelerant.com>
2025-02-07 17:33:51 +0530

    Finished Gatsby Tutorial Part 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,18 +1,20 @@
 import * as React from "react"
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import { graphql } from "gatsby"
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+import { Link, graphql } from "gatsby"
 
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            <p>My cool posts will go in here</p>
             {
                 data.allMdx.nodes.map((node) => (
                 <article key={node.id}>
-                    <h2>{node.frontmatter.title}</h2>
+                    <h2>
+                    <Link to={`/blog/${node.frontmatter.slug}`}>
+                      {node.frontmatter.title}
+                    </Link>
+                  </h2>
                     <p>Posted: {node.frontmatter.date}</p>
-                    <p>{node.excerpt}</p>
                 </article>
                 ))
             }
@@ -27,6 +29,7 @@ export const data = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
         excerpt
---
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
+import { graphql } from "gatsby"
+
+const BlogPost = ({ data, children }) => {
+  return (
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      {children}
+    </Layout>
+  )
+}
+
+export const data = graphql`
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      excerpt
+    }
+  }
+`
+
+export const Head = () => <Seo title="Super Cool Blog Posts" />
+
+export default BlogPost
